c0b6a6f0650836932782c19fe8ea05b8f993f2ca
Fedir kantur <fedichk@gmail.com>
2017-06-16 21:26:15 +0300

    Made MyData domain

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,11 @@
 import demograils.Counter
+import demograils.MyData
 
 class BootStrap {
 
     def init = { servletContext ->
         if (Counter.findByName("QN_ID") == null) {
-            new Counter(name: "QN_ID", value: 0).save()
+            new Counter(name: "QN_ID").save()
         }
     }
     def destroy = {
